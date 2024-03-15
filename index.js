a27e71c7b7db9b3cfaee1663f23203ce3168f395
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-03-15 12:16:21 +0200

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+const navBar = document.querySelector(".fa-bars");
+const hideMenu = document.querySelector(".menu");
+
+navBar.addEventListener("click", () => {
+  hideMenu.classList.toggle("show-menu");
+});
+
+
